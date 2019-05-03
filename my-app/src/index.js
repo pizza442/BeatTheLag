db1b0e85f65379f02c64e3e75241e99c7f7375e2
Vanely Ruiz <VanelyRuiz@gmail.com>
2019-05-02 18:19:03 -0700

    Added firebase config

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDGFApTkLg7HaIRjBrATMk3z-INzvUsIis",
+  authDomain: "beatthelag-info442.firebaseapp.com",
+  databaseURL: "https://beatthelag-info442.firebaseio.com",
+  projectId: "beatthelag-info442",
+  storageBucket: "beatthelag-info442.appspot.com",
+  messagingSenderId: "214643886468"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')

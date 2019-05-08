4dbebafe699b7497847c2915d3249b4ca8c50fc4
Sandy Ynag <yang21@uw.edu>
2019-05-07 23:58:15 -0700

    add sudowudoCodeeeeee

--- my-app/src/Schedule.js
+++ my-app/src/Schedule.js
@@ -8,19 +8,29 @@ export class Schedule {
     constructor (DepartureLocation, DepartureTime, DepartureDate,
                  ArrivalLocation, ArrivalTime, ArriveDate,
                  NormalSleepTime, NormalWakeTime) {
-        
+
         this.DepartureLocation = DepartureLocation;
         this.DepartureTime = DepartureTime;
         this.DepartureDate = DepartureDate;
-        
+
         this.ArrivalLocation = ArrivalLocation;
         this.ArrivalTime = ArrivalTime;
         this.ArriveDate = ArriveDate;
-        
+
         this.NormalSleepTime = NormalSleepTime;
         this.NormalWakeTime = NormalWakeTime;
-                 
-                
-                
+
+        // totalDays = DepartureDate - ArriveDate
+        // sleepingLength =  NormalWakeTime - NormalSleepTime
+        // length of schedule array should be totalDays
+        // secheduleStartTime = NormalSleepTime
+        // if (totalDays > 0) {
+        //    User will sleep earlier and earlier: 11 -> 10 -> 9
+        // } else if (totalDays < 0) {
+        //    User will sleep earlier and earlier: 11 -> 12 -> 1
+        // } else {
+        //   "You don't need this page what are you doing"
+        // }
+
     }
 }

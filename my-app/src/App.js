0dc3c6d0d4b361ae25192ba7281c06ad191d45e1
aliar97 <aliar97@uw.edu>
2019-05-07 17:27:58 -0700

    new branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Fragment} from 'react';
+import {Card} from 'mdbreact';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <Fragment>
+                <div>BeatTheLag</div>
+                <Card className="content_holder">
+          
+                        <div className="users-list-title">
+                        <a onClick={() => this.openInNewTab()}>Sign In</a>
+                        </div>
+
+                       
+                </Card>
+                </Fragment>
       </div>
     );
   }
